bba85e50b593212c5cfce95f1de0ec27568313a2
improvola <83605546+improvola@users.noreply.github.com>
2022-11-14 20:07:09 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+function show() {
+  alert("You clicked on the button, so it triggered the alert box");
+}
+
 $(".size").on('click', function(){
     $(this).toggleClass('focus').siblings().removeClass('focus');
  })
